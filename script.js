82948d59167c25348a1f84bb36a72e939a73edbb
Nicolas Danciu <nixrom1@gmail.com>
2022-02-01 14:14:26 -0500

    Making great progress with the formatting of the page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Weather API
+
 fetch("https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=fee0f647577d0bbdd19bf3b2efcb8ace",)
  .then(function (response) {
      return response.json();
@@ -9,7 +11,9 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=fee0f647
           console.log("there was a problem " , error);
       });
       
-      fetch("https://api.kanye.rest",)
+//Kanye quote API
+
+fetch("https://api.kanye.rest",)
  .then(function (response) {
      return response.json();
   } )
